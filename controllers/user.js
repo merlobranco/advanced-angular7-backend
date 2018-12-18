a795c5a7f2c38f7df05b8d4159b7b05d8cad9393
Brais Cidras <braisomary@gmail.com>
2018-12-18 17:52:31 +0200

    Adding User Create Action Handler

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,56 @@
 'use strict'
 
+// Node modules
+var bcrypt = require('bcrypt-nodejs');
+
+// Models
+var User = require('../models/user');
+
+// Actions
 function tests(req, res) {
 	res.status(200).send({
 		message: 'Testing the user controller and tests action'
 	});
 }
 
+function create(req, res) {
+	// Creating User Object
+	var user = new User();
+
+	// Getting the received parameters
+	var params = req.body;
+
+	if(params.name && params.surname && params.email && params.password) {
+		// Setting User property values
+		user.name = params.name;
+		user.surname = params.surname;
+		user.email = params.email;
+		user.role = 'ROLE_USER';
+		user.image = null;
+
+		// Encrypting the password
+		bcrypt.hash(params.password, null, null, function(err, hash) {
+			user.password = hash;
+
+			// Saving the user
+			user.save((err, userStored) => {
+				if (err) {
+					res.status(500).send({message: 'Thrown error while creating the user'});
+				} else {
+					if (!userStored) {
+						res.status(404).send({message: 'User has not been created'});
+					} else {
+						res.status(200).send({user: userStored});
+					}
+				}
+			});
+		});
+	} else {
+		res.status(200).send({message: 'Please provide the required User data'});
+	}
+}
+
 module.exports = {
-	tests
+	tests,
+	create
 };
--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,6 @@ var UserController = require('../controllers/user');
 var api = express.Router();
 
 api.get('/controller-tests', UserController.tests);
+api.post('/create', UserController.create);
 
 module.exports = api;
