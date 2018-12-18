9d8dbe56a0bfef4311a6bf97edc503451dc83437
Brais Cidras <braisomary@gmail.com>
2018-12-18 19:00:45 +0200

    Adding User Duplication Control

--- controllers/user.js
+++ controllers/user.js
@@ -28,23 +28,35 @@ function create(req, res) {
 		user.role = 'ROLE_USER';
 		user.image = null;
 
-		// Encrypting the password
-		bcrypt.hash(params.password, null, null, function(err, hash) {
-			user.password = hash;
+		User.findOne({email: user.email.toLowerCase()}, (err, isSetUser) => { 
+			if (err) {
+				res.status(500).send({message: 'Thrown error while checking if the user exists'});
+			} else {
+				if (!isSetUser) {
+					// Encrypting the password
+					bcrypt.hash(params.password, null, null, function(err, hash) {
+						user.password = hash;
 
-			// Saving the user
-			user.save((err, userStored) => {
-				if (err) {
-					res.status(500).send({message: 'Thrown error while creating the user'});
+						// Saving the user
+						user.save((err, userStored) => {
+							if (err) {
+								res.status(500).send({message: 'Thrown error while creating the user'});
+							} else {
+								if (!userStored) {
+									res.status(404).send({message: 'User has not been created'});
+								} else {
+									res.status(200).send({user: userStored});
+								}
+							}
+						});
+					});
 				} else {
-					if (!userStored) {
-						res.status(404).send({message: 'User has not been created'});
-					} else {
-						res.status(200).send({user: userStored});
-					}
+					res.status(200).send({message: 'The user cannot be created because already exists'});
 				}
-			});
+			}
 		});
+
+		
 	} else {
 		res.status(200).send({message: 'Please provide the required User data'});
 	}
