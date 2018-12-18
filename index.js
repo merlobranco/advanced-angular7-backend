0b42f5adf6bdb3dc47d6cbf3f4d78e139fe89871
Brais Cidras <braisomary@gmail.com>
2018-12-18 13:24:34 +0200

    Connecting to the MongoDB with Node.js

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/zoo', {useNewUrlParser: true})
+	.then(() => {
+		console.log('La conexión a la base de datos se ha realizado correctamente...');
+	})
+	.catch(err => console.log(err));
