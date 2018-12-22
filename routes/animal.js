a2fc0d7fe4c563b2311755e1669f0d7fd8ecafeb
Brais Cidras <braisomary@gmail.com>
2018-12-22 21:16:33 +0200

    Adding Administrator Role Middleware

--- routes/animal.js
+++ routes/animal.js
@@ -5,17 +5,19 @@ var AnimalController = require('../controllers/animal');
 
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
+var md_admin = require('../middlewares/is_admin');
 
 var multipart = require('connect-multiparty');
 var md_upload = multipart({ uploadDir: './uploads/animals'});
 
 api.get('/animals/controller-tests', md_auth.ensureAuth, AnimalController.tests);
-api.post('/animals/create', md_auth.ensureAuth, AnimalController.create);
 api.get('/animals/', AnimalController.getAnimals);
 api.get('/animals/:id', AnimalController.getAnimal);
-api.put('/animals/:id', md_auth.ensureAuth, AnimalController.update);
-api.post('/animals/upload-image/:id', [md_auth.ensureAuth, md_upload], AnimalController.uploadImage);
+api.post('/animals/create', [md_auth.ensureAuth, md_admin.isAdmin], AnimalController.create);
+api.put('/animals/:id', [md_auth.ensureAuth, md_admin.isAdmin], AnimalController.update);
+api.delete('/animals/:id', [md_auth.ensureAuth, md_admin.isAdmin], AnimalController.remove);
+
+api.post('/animals/upload-image/:id', [md_auth.ensureAuth, md_admin.isAdmin, md_upload], AnimalController.uploadImage);
 api.get('/animals/get-image-file/:imageFile', AnimalController.getImageFile);
-api.delete('/animals/:id', md_auth.ensureAuth, AnimalController.remove);
 
 module.exports = api;
--- routes/user.js
+++ routes/user.js
@@ -10,11 +10,13 @@ var multipart = require('connect-multiparty');
 var md_upload = multipart({ uploadDir: './uploads/users'});
 
 api.get('/controller-tests', md_auth.ensureAuth, UserController.tests);
+api.get('/keepers', UserController.getKeepers);
 api.post('/create', UserController.create);
 api.post('/login', UserController.login);
 api.put('/update/:id', md_auth.ensureAuth, UserController.update);
+
 api.post('/upload-image/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
 api.get('/get-image-file/:imageFile', UserController.getImageFile);
-api.get('/keepers', UserController.getKeepers);
+
 
 module.exports = api;
